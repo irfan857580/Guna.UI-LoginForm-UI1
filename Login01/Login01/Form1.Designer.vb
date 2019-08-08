7638d38468c6e67018bb9e418f38f8a53eb415ec
Sobatdata Software <sobatdata.software@gmail.com>
2019-08-08 12:16:44 +0700

    Init

---
+++ Login01/Login01/Form1.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.GunaShadowPanel1 = New Guna.UI.WinForms.GunaShadowPanel()
+        Me.GunaButton3 = New Guna.UI.WinForms.GunaButton()
+        Me.GunaButton2 = New Guna.UI.WinForms.GunaButton()
+        Me.GunaCircleButton1 = New Guna.UI.WinForms.GunaCircleButton()
+        Me.GunaPanel1 = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaButton1 = New Guna.UI.WinForms.GunaButton()
+        Me.GunaTextBox2 = New Guna.UI.WinForms.GunaTextBox()
+        Me.GunaTextBox1 = New Guna.UI.WinForms.GunaTextBox()
+        Me.GunaPictureBox1 = New Guna.UI.WinForms.GunaPictureBox()
+        Me.GunaLabel1 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaImageButton1 = New Guna.UI.WinForms.GunaImageButton()
+        Me.GunaLinkLabel1 = New Guna.UI.WinForms.GunaLinkLabel()
+        Me.GunaSeparator1 = New Guna.UI.WinForms.GunaSeparator()
+        Me.GunaLinkLabel2 = New Guna.UI.WinForms.GunaLinkLabel()
+        Me.GunaDragControl1 = New Guna.UI.WinForms.GunaDragControl(Me.components)
+        Me.GunaShadowPanel1.SuspendLayout()
+        Me.GunaPanel1.SuspendLayout()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'GunaShadowPanel1
+        '
+        Me.GunaShadowPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaShadowPanel1.BaseColor = System.Drawing.Color.Transparent
+        Me.GunaShadowPanel1.Controls.Add(Me.GunaCircleButton1)
+        Me.GunaShadowPanel1.Location = New System.Drawing.Point(167, 300)
+        Me.GunaShadowPanel1.Name = "GunaShadowPanel1"
+        Me.GunaShadowPanel1.Radius = 15
+        Me.GunaShadowPanel1.ShadowColor = System.Drawing.Color.Black
+        Me.GunaShadowPanel1.ShadowDepth = 40
+        Me.GunaShadowPanel1.Size = New System.Drawing.Size(64, 68)
+        Me.GunaShadowPanel1.TabIndex = 1
+        Me.GunaShadowPanel1.UseTransfarantBackground = True
+        '
+        'GunaButton3
+        '
+        Me.GunaButton3.AnimationHoverSpeed = 0.07!
+        Me.GunaButton3.AnimationSpeed = 0.03!
+        Me.GunaButton3.BaseColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(177, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.GunaButton3.BorderColor = System.Drawing.Color.Black
+        Me.GunaButton3.Font = New System.Drawing.Font("Segoe UI", 10.5!)
+        Me.GunaButton3.ForeColor = System.Drawing.Color.White
+        Me.GunaButton3.Image = Global.Login01.My.Resources.Resources.Twitter_24px
+        Me.GunaButton3.ImageSize = New System.Drawing.Size(24, 24)
+        Me.GunaButton3.Location = New System.Drawing.Point(72, 440)
+        Me.GunaButton3.Name = "GunaButton3"
+        Me.GunaButton3.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(47, Byte), Integer), CType(CType(118, Byte), Integer), CType(CType(162, Byte), Integer))
+        Me.GunaButton3.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.GunaButton3.OnHoverForeColor = System.Drawing.Color.White
+        Me.GunaButton3.OnHoverImage = Nothing
+        Me.GunaButton3.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaButton3.Radius = 6
+        Me.GunaButton3.Size = New System.Drawing.Size(255, 43)
+        Me.GunaButton3.TabIndex = 3
+        Me.GunaButton3.Text = "Login With Twitter"
+        '
+        'GunaButton2
+        '
+        Me.GunaButton2.AnimationHoverSpeed = 0.07!
+        Me.GunaButton2.AnimationSpeed = 0.03!
+        Me.GunaButton2.BaseColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(133, Byte), Integer), CType(CType(198, Byte), Integer))
+        Me.GunaButton2.BorderColor = System.Drawing.Color.Black
+        Me.GunaButton2.Font = New System.Drawing.Font("Segoe UI", 10.5!)
+        Me.GunaButton2.ForeColor = System.Drawing.Color.White
+        Me.GunaButton2.Image = Global.Login01.My.Resources.Resources.Facebook_F_24px
+        Me.GunaButton2.ImageSize = New System.Drawing.Size(24, 24)
+        Me.GunaButton2.Location = New System.Drawing.Point(72, 385)
+        Me.GunaButton2.Name = "GunaButton2"
+        Me.GunaButton2.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(132, Byte), Integer))
+        Me.GunaButton2.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.GunaButton2.OnHoverForeColor = System.Drawing.Color.White
+        Me.GunaButton2.OnHoverImage = Nothing
+        Me.GunaButton2.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaButton2.Radius = 6
+        Me.GunaButton2.Size = New System.Drawing.Size(255, 43)
+        Me.GunaButton2.TabIndex = 2
+        Me.GunaButton2.Text = "Login With Facebook"
+        '
+        'GunaCircleButton1
+        '
+        Me.GunaCircleButton1.AnimationHoverSpeed = 0.07!
+        Me.GunaCircleButton1.AnimationSpeed = 0.03!
+        Me.GunaCircleButton1.BaseColor = System.Drawing.Color.White
+        Me.GunaCircleButton1.BorderColor = System.Drawing.Color.Black
+        Me.GunaCircleButton1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.GunaCircleButton1.ForeColor = System.Drawing.Color.White
+        Me.GunaCircleButton1.Image = Global.Login01.My.Resources.Resources.Expand_Arrow_32px
+        Me.GunaCircleButton1.ImageSize = New System.Drawing.Size(32, 32)
+        Me.GunaCircleButton1.Location = New System.Drawing.Point(0, 0)
+        Me.GunaCircleButton1.Name = "GunaCircleButton1"
+        Me.GunaCircleButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(151, Byte), Integer), CType(CType(143, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaCircleButton1.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.GunaCircleButton1.OnHoverForeColor = System.Drawing.Color.White
+        Me.GunaCircleButton1.OnHoverImage = Global.Login01.My.Resources.Resources.Expand_Arrow_hover_32px
+        Me.GunaCircleButton1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaCircleButton1.Size = New System.Drawing.Size(64, 64)
+        Me.GunaCircleButton1.TabIndex = 2
+        '
+        'GunaPanel1
+        '
+        Me.GunaPanel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(207, Byte), Integer), CType(CType(250, Byte), Integer), CType(CType(247, Byte), Integer))
+        Me.GunaPanel1.BackgroundImage = Global.Login01.My.Resources.Resources.ccc
+        Me.GunaPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.GunaPanel1.Controls.Add(Me.GunaButton1)
+        Me.GunaPanel1.Controls.Add(Me.GunaTextBox2)
+        Me.GunaPanel1.Controls.Add(Me.GunaTextBox1)
+        Me.GunaPanel1.Controls.Add(Me.GunaPictureBox1)
+        Me.GunaPanel1.Controls.Add(Me.GunaLabel1)
+        Me.GunaPanel1.Controls.Add(Me.GunaImageButton1)
+        Me.GunaPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GunaPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.GunaPanel1.Name = "GunaPanel1"
+        Me.GunaPanel1.Size = New System.Drawing.Size(390, 320)
+        Me.GunaPanel1.TabIndex = 0
+        '
+        'GunaButton1
+        '
+        Me.GunaButton1.AnimationHoverSpeed = 0.07!
+        Me.GunaButton1.AnimationSpeed = 0.03!
+        Me.GunaButton1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaButton1.BaseColor = System.Drawing.Color.Transparent
+        Me.GunaButton1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(239, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(247, Byte), Integer))
+        Me.GunaButton1.BorderSize = 1
+        Me.GunaButton1.Font = New System.Drawing.Font("Segoe UI", 14.0!)
+        Me.GunaButton1.ForeColor = System.Drawing.Color.White
+        Me.GunaButton1.Image = Nothing
+        Me.GunaButton1.ImageSize = New System.Drawing.Size(20, 20)
+        Me.GunaButton1.Location = New System.Drawing.Point(148, 239)
+        Me.GunaButton1.Name = "GunaButton1"
+        Me.GunaButton1.OnHoverBaseColor = System.Drawing.Color.White
+        Me.GunaButton1.OnHoverBorderColor = System.Drawing.Color.White
+        Me.GunaButton1.OnHoverForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(233, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.GunaButton1.OnHoverImage = Nothing
+        Me.GunaButton1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaButton1.OnPressedDepth = 0
+        Me.GunaButton1.Radius = 6
+        Me.GunaButton1.Size = New System.Drawing.Size(103, 36)
+        Me.GunaButton1.TabIndex = 5
+        Me.GunaButton1.Text = "Go !"
+        Me.GunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GunaTextBox2
+        '
+        Me.GunaTextBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GunaTextBox2.BaseColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.GunaTextBox2.BorderColor = System.Drawing.Color.Silver
+        Me.GunaTextBox2.BorderSize = 0
+        Me.GunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.GunaTextBox2.FocusedBaseColor = System.Drawing.Color.White
+        Me.GunaTextBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText
+        Me.GunaTextBox2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.GunaTextBox2.ForeColor = System.Drawing.Color.Gray
+        Me.GunaTextBox2.Location = New System.Drawing.Point(72, 172)
+        Me.GunaTextBox2.Name = "GunaTextBox2"
+        Me.GunaTextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.GunaTextBox2.Radius = 6
+        Me.GunaTextBox2.Size = New System.Drawing.Size(255, 37)
+        Me.GunaTextBox2.TabIndex = 4
+        Me.GunaTextBox2.Text = "password"
+        Me.GunaTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GunaTextBox1
+        '
+        Me.GunaTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaTextBox1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.GunaTextBox1.BorderColor = System.Drawing.Color.Silver
+        Me.GunaTextBox1.BorderSize = 0
+        Me.GunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.GunaTextBox1.FocusedBaseColor = System.Drawing.Color.White
+        Me.GunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText
+        Me.GunaTextBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.GunaTextBox1.ForeColor = System.Drawing.Color.Gray
+        Me.GunaTextBox1.Location = New System.Drawing.Point(72, 124)
+        Me.GunaTextBox1.Name = "GunaTextBox1"
+        Me.GunaTextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.GunaTextBox1.Radius = 6
+        Me.GunaTextBox1.Size = New System.Drawing.Size(255, 37)
+        Me.GunaTextBox1.TabIndex = 3
+        Me.GunaTextBox1.Text = "@username"
+        Me.GunaTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GunaPictureBox1
+        '
+        Me.GunaPictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaPictureBox1.Image = Global.Login01.My.Resources.Resources.logo5
+        Me.GunaPictureBox1.Location = New System.Drawing.Point(326, 21)
+        Me.GunaPictureBox1.Name = "GunaPictureBox1"
+        Me.GunaPictureBox1.Size = New System.Drawing.Size(32, 32)
+        Me.GunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.GunaPictureBox1.TabIndex = 2
+        Me.GunaPictureBox1.TabStop = False
+        '
+        'GunaLabel1
+        '
+        Me.GunaLabel1.AutoSize = True
+        Me.GunaLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaLabel1.Font = New System.Drawing.Font("Segoe UI", 18.0!)
+        Me.GunaLabel1.ForeColor = System.Drawing.Color.White
+        Me.GunaLabel1.Location = New System.Drawing.Point(161, 75)
+        Me.GunaLabel1.Name = "GunaLabel1"
+        Me.GunaLabel1.Size = New System.Drawing.Size(83, 32)
+        Me.GunaLabel1.TabIndex = 1
+        Me.GunaLabel1.Text = "LOGIN"
+        '
+        'GunaImageButton1
+        '
+        Me.GunaImageButton1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaImageButton1.Image = Global.Login01.My.Resources.Resources.Menu2_24px
+        Me.GunaImageButton1.ImageSize = New System.Drawing.Size(24, 24)
+        Me.GunaImageButton1.Location = New System.Drawing.Point(12, 12)
+        Me.GunaImageButton1.Name = "GunaImageButton1"
+        Me.GunaImageButton1.OnHoverImage = Nothing
+        Me.GunaImageButton1.OnHoverImageOffset = New System.Drawing.Point(0, -2)
+        Me.GunaImageButton1.Size = New System.Drawing.Size(30, 30)
+        Me.GunaImageButton1.TabIndex = 0
+        '
+        'GunaLinkLabel1
+        '
+        Me.GunaLinkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaLinkLabel1.AutoSize = True
+        Me.GunaLinkLabel1.Font = New System.Drawing.Font("Segoe UI", 10.5!)
+        Me.GunaLinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
+        Me.GunaLinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GunaLinkLabel1.Location = New System.Drawing.Point(133, 502)
+        Me.GunaLinkLabel1.Name = "GunaLinkLabel1"
+        Me.GunaLinkLabel1.Size = New System.Drawing.Size(122, 19)
+        Me.GunaLinkLabel1.TabIndex = 4
+        Me.GunaLinkLabel1.TabStop = True
+        Me.GunaLinkLabel1.Text = "Create an Account"
+        '
+        'GunaSeparator1
+        '
+        Me.GunaSeparator1.LineColor = System.Drawing.Color.Gainsboro
+        Me.GunaSeparator1.Location = New System.Drawing.Point(89, 528)
+        Me.GunaSeparator1.Name = "GunaSeparator1"
+        Me.GunaSeparator1.Size = New System.Drawing.Size(217, 10)
+        Me.GunaSeparator1.TabIndex = 5
+        '
+        'GunaLinkLabel2
+        '
+        Me.GunaLinkLabel2.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaLinkLabel2.AutoSize = True
+        Me.GunaLinkLabel2.Font = New System.Drawing.Font("Segoe UI", 7.0!)
+        Me.GunaLinkLabel2.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
+        Me.GunaLinkLabel2.LinkColor = System.Drawing.Color.Gray
+        Me.GunaLinkLabel2.Location = New System.Drawing.Point(147, 546)
+        Me.GunaLinkLabel2.Name = "GunaLinkLabel2"
+        Me.GunaLinkLabel2.Size = New System.Drawing.Size(87, 12)
+        Me.GunaLinkLabel2.TabIndex = 6
+        Me.GunaLinkLabel2.TabStop = True
+        Me.GunaLinkLabel2.Text = "ABOUT | CONTACT"
+        '
+        'GunaDragControl1
+        '
+        Me.GunaDragControl1.TargetControl = Me.GunaPanel1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(239, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(247, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(390, 580)
+        Me.Controls.Add(Me.GunaLinkLabel2)
+        Me.Controls.Add(Me.GunaSeparator1)
+        Me.Controls.Add(Me.GunaLinkLabel1)
+        Me.Controls.Add(Me.GunaButton3)
+        Me.Controls.Add(Me.GunaButton2)
+        Me.Controls.Add(Me.GunaShadowPanel1)
+        Me.Controls.Add(Me.GunaPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.GunaShadowPanel1.ResumeLayout(False)
+        Me.GunaPanel1.ResumeLayout(False)
+        Me.GunaPanel1.PerformLayout()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GunaPanel1 As Guna.UI.WinForms.GunaPanel
+    Friend WithEvents GunaImageButton1 As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents GunaPictureBox1 As Guna.UI.WinForms.GunaPictureBox
+    Friend WithEvents GunaLabel1 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaButton1 As Guna.UI.WinForms.GunaButton
+    Friend WithEvents GunaTextBox2 As Guna.UI.WinForms.GunaTextBox
+    Friend WithEvents GunaTextBox1 As Guna.UI.WinForms.GunaTextBox
+    Friend WithEvents GunaShadowPanel1 As Guna.UI.WinForms.GunaShadowPanel
+    Friend WithEvents GunaCircleButton1 As Guna.UI.WinForms.GunaCircleButton
+    Friend WithEvents GunaButton2 As Guna.UI.WinForms.GunaButton
+    Friend WithEvents GunaButton3 As Guna.UI.WinForms.GunaButton
+    Friend WithEvents GunaLinkLabel2 As Guna.UI.WinForms.GunaLinkLabel
+    Friend WithEvents GunaSeparator1 As Guna.UI.WinForms.GunaSeparator
+    Friend WithEvents GunaLinkLabel1 As Guna.UI.WinForms.GunaLinkLabel
+    Friend WithEvents GunaDragControl1 As Guna.UI.WinForms.GunaDragControl
+End Class
